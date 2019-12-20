680a43aeaa836a259bdc1c19ef8a3687c5160ad9
Sofia Levin <sofia@Joels-MacBook-Pro.local>
2019-12-20 08:20:41 -0800

    Completed POST /register endpoint.

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,45 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/usersModel.js');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  const token = signToken(user);
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json({
+        token: token,
+        message: `Welcome ${user.username}!`,
+      });
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
   // implement login
 });
+
+function signToken(user) {
+  const payload = {
+    username: user.username,
+    department: user.department
+  };
+
+  const secret = process.env.JWT_SECRET || "shhhhh, secret!";
+
+  const options = {
+    expiresIn: "24h"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
